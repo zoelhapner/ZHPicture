b3a788b6453bf0b709da0fed8020eec1c11784cf
zoelhapner <achmadzulkiflinurrochim@gmail.com>
2026-08-11 16:36:04 +0700

    salah update tabelllssss

--- database/migrations/2025_04_04_080000_create_users_table.php
+++ database/migrations/2025_04_04_080000_create_users_table.php
@@ -62,6 +62,5 @@ return new class extends Migration
     {
         Schema::dropIfExists('users');
         Schema::dropIfExists('password_reset_tokens');
-        Schema::dropIfExists('sessions');
     }
 };
